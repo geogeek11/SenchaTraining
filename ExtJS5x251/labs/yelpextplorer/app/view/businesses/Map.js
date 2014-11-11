7724adf1a9b0c5cdcddaac639987a2fddca08dbd
Bradley Wogsland <bwogsland@leadspedia.com>
2014-11-11 12:28:54 -0600

    YelpExtplorer 3

--- ExtJS5x251/labs/yelpextplorer/app/view/businesses/Map.js
+++ ExtJS5x251/labs/yelpextplorer/app/view/businesses/Map.js
@@ -1,7 +1,4 @@
 Ext.define('YelpExtplorer.view.businesses.Map', {
-  extend: 'Ext.panel.Panel',
-  xtype: 'businessesmap',
-
-  html: 'der map'
-
+  extend: 'Shared.view.Map',
+  xtype: 'businessesmap'
 });
