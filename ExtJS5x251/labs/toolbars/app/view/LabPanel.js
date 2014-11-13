145e5a1dc0ccb064a40f938cce2fe78d73257383
Bradley Wogsland <bwogsland@leadspedia.com>
2014-11-13 09:04:33 -0600

    toolbars

--- ExtJS5x251/labs/toolbars/app/view/LabPanel.js
+++ ExtJS5x251/labs/toolbars/app/view/LabPanel.js
@@ -4,15 +4,30 @@ Ext.define('Toolbars.view.LabPanel', {
 	title: 'My Panel',
 	height: 200,
 	width: 340,
-	padding: 16
-
-
-
-
-
-
-
-
-	
-
+	padding: 16,
+	dockedItems: [{
+		xtype: 'toolbar',
+		dock: 'top',
+		items: [{
+			text: 'Eins!'
+		}]
+	},{
+		xtype: 'toolbar',
+		dock: 'top',
+		items: [{
+			text: 'Zwei!'
+		}]
+	}],
+	/*tbar: [{
+		text: 'One'
+	}],*/
+	bbar: [{
+		text: 'bbar'
+	}],
+	lbar: [{
+		text: 'lbar'
+	}],
+	rbar: [{
+		text: 'rbar'
+	}]
 });
